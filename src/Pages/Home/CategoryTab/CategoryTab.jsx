3636fdf1af6919b5c521a805d95cd6f07522b1f5
samsulalamasif <samsulalamasif@gmail.com>
2023-05-19 08:38:00 +0600

    shop category title add

--- src/Pages/Home/CategoryTab/CategoryTab.jsx
+++ src/Pages/Home/CategoryTab/CategoryTab.jsx
@@ -19,6 +19,14 @@ const CategoryTab = () => {
 
         <div data-aos="zoom-in-up"
             data-aos-duration="2000">
+            <div className='text-center my-24'>
+                <h1 className='mb-5 text-4xl font-bold font-serif'>Our Shop Category</h1>
+                <p className="mb-5">Robotics is a branch of engineering that involves <br />
+                    the conception, design, manufacture and operation of robots. The objective of the
+                    <br />
+                    robotics field is to create intelligent machines that can assist humans in a variety <br />
+                    of ways. Robotics can take on a number of forms.</p>
+            </div>
             <Tabs>
                 <TabList className="w-1/2 text-xl mx-auto text-orange-600 font-serif font-bold text-center">
                     <Tab >Humanoid Robotics</Tab>
